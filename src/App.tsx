d3fd03b1f3105673a72eb66208aff2b82109add0
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-07-02 18:41:25 +0300

    added "add user"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState<Array<{ id: number | string, name: string, banned: boolean }>>([])
+
+    const getUsers = () => {
+        axios.get('http://localhost:5000/users')
+            .then((res) => {
+                setUsers(res.data)
+            })
+    }
+
+    const onClickHandler = () => {
+        axios.post('http://localhost:5000/users',)
+            .then(res => {
+                    console.log(res.data)
+                    getUsers()
+                }
+            )
+    }
+
+    const onChangeHandler = (userId: number | string) => {
+        axios.put('http://localhost:5000/users', {userId})
+            .then(res => {
+                console.log(res.data)
+                getUsers()
+            })
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    return (
+        <div style={{display: 'flex'}}>
+            <div>
+                {users.map(u =>
+                    <div key={u.id}>Name: {u.name}, Banned: <input type={'checkbox'}
+                                                                   onChange={() => onChangeHandler(u.id)}
+                                                                   checked={u.banned}/>
+                    </div>)
+                }
+            </div>
+            <div>
+                <button onClick={onClickHandler}>Add user</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
