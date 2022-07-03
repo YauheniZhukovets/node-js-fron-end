c51c9d92758759e7c3a0802f8a00b8c497ae94ef
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-07-03 18:54:35 +0300

    created userNameRef, userBannedRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import axios from 'axios';
 
 
 function App() {
     const [users, setUsers] = useState<Array<{ id: number | string, name: string, banned: boolean }>>([])
+    const userNameRef = useRef<HTMLInputElement | null>(null)
+    const userBannedRef = useRef<HTMLInputElement | null>(null)
 
     const getUsers = () => {
         axios.get('http://localhost:5000/users')
@@ -14,20 +16,15 @@ function App() {
     }
 
     const onClickHandler = () => {
-        axios.post('http://localhost:5000/users',)
+        axios.post('http://localhost:5000/users', {
+            name: userNameRef.current?.value,
+            banned: userBannedRef.current?.checked
+        })
             .then(res => {
                     console.log(res.data)
                     getUsers()
                 }
             )
-    }
-
-    const onChangeHandler = (userId: number | string) => {
-        axios.put('http://localhost:5000/users', {userId})
-            .then(res => {
-                console.log(res.data)
-                getUsers()
-            })
     }
 
     useEffect(() => {
@@ -39,12 +36,13 @@ function App() {
             <div>
                 {users.map(u =>
                     <div key={u.id}>Name: {u.name}, Banned: <input type={'checkbox'}
-                                                                   onChange={() => onChangeHandler(u.id)}
                                                                    checked={u.banned}/>
                     </div>)
                 }
             </div>
             <div>
+                <input type="text" ref={userNameRef}/>
+                <input type={'checkbox'} ref={userBannedRef}/>
                 <button onClick={onClickHandler}>Add user</button>
             </div>
         </div>
