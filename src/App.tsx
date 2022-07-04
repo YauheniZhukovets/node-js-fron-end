91249c219f05d76dbf083575e9b1c445b504ff63
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-07-04 23:13:25 +0300

    added the ability to delete users

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,36 @@ import axios from 'axios';
 
 
 function App() {
-    const [users, setUsers] = useState<Array<{ id: number | string, name: string, banned: boolean }>>([])
-    const userNameRef = useRef<HTMLInputElement | null>(null)
-    const userBannedRef = useRef<HTMLInputElement | null>(null)
+    const [users, setUsers] = useState<Array<{ _id: string, name: string, banned: boolean }>>([])
+    let userNameRef = useRef<HTMLInputElement | null>(null)
+    let userBannedRef = useRef<HTMLInputElement | null>(null)
+
+
 
     const getUsers = () => {
-        axios.get('http://localhost:5000/users')
+        axios.get('http://localhost:5000/users/' + window.location.search )
             .then((res) => {
                 setUsers(res.data)
+
             })
     }
 
-    const onClickHandler = () => {
-        axios.post('http://localhost:5000/users', {
+    const createUser = () => {
+        axios.post('http://localhost:5000/users/', {
             name: userNameRef.current?.value,
             banned: userBannedRef.current?.checked
         })
+            .then(res => {
+                    console.log(res.data)
+                    getUsers()
+                }
+            )
+        userBannedRef.current!.checked = false
+        userNameRef.current!.value = '';
+    }
+
+    const deleteUser = (userId: string) => {
+        axios.delete(`http://localhost:5000/users/${userId}`)
             .then(res => {
                     console.log(res.data)
                     getUsers()
@@ -35,15 +49,16 @@ function App() {
         <div style={{display: 'flex'}}>
             <div>
                 {users.map(u =>
-                    <div key={u.id}>Name: {u.name}, Banned: <input type={'checkbox'}
-                                                                   checked={u.banned}/>
+                    <div key={u._id}>Name: {u.name},
+                        Banned: <input type={'checkbox'} checked={u.banned}/>
+                        <button onClick={() => deleteUser(u._id)}>X</button>
                     </div>)
                 }
             </div>
             <div>
                 <input type="text" ref={userNameRef}/>
                 <input type={'checkbox'} ref={userBannedRef}/>
-                <button onClick={onClickHandler}>Add user</button>
+                <button onClick={createUser}>Add user</button>
             </div>
         </div>
     );
